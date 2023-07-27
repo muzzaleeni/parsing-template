26c5796d3fcf1859f700c3fe25a468edc3ad83c3
Muslim Bayemirov <muslimbayemirov@gmail.com>
2023-07-27 09:04:56 +0600

    Added dynamic loading overcoming solution

--- README.md
+++ README.md
@@ -1,44 +1,38 @@
-# Web Scraping Additional Information from 2GIS
+# Web Scraping Script for 2GIS Reviews
 
-This script allows you to scrape additional information from webpages using the 2GIS website. It takes an input CSV file containing URLs and adds a new column with the scraped information to the file.
+This script performs web scraping to extract reviews from 2GIS (a popular local search service) for a given list of URLs and adds the reviews from the last 6 months to the input CSV file. Note that page is using dynamic loading.
 
-## Requirements
+## Prerequisites
 
-- Python 3.x
-- `requests` library
-- `beautifulsoup4` library
-- `csv` library
+- Python 3.6 or above is required to run the script.
+- Make sure you have installed the required Python packages mentioned in the `requirements.txt`.
 
 ## Installation
 
-1. Make sure you have Python 3.x installed on your system.
-2. Install the required libraries by running the following command:
+1. Clone this repository to your local machine or download the script `scrape_2gis_reviews.py`.
 
-`pip install requests beautifulsoup4`
+2. Install the required Python packages by running the following command:
 
+3. Prepare the input CSV file with the list of businesses and their 2GIS URLs. The CSV file should have the following columns:
+
+   - `Name`: Name of the business
+   - `2GIS URL`: URL of the business on 2GIS
+
+4. Update the `input_file` and `output_file` variables in the script to point to the correct paths of your input CSV file and the output file.
+
+5. Optionally, you can set the `new_column` variable to specify the name of the new column where the extracted reviews will be added in the CSV file.
 
 ## Usage
 
-1. Prepare your input CSV file (`results.csv`) with a column named `2GIS URL` containing the URLs of the webpages you want to scrape additional information from.
-2. Place the script (`script.py`) in the same directory as your input CSV file.
-3. Open a terminal or command prompt and navigate to the directory containing the script.
-4. Run the script by executing the following command:
+To run the script, use the following command in your terminal or command prompt:
 
-`python script.py`
-
-
-5. The script will start scraping the additional information from the webpages. It will print the row number and name of each processed restaurant to track the progress.
-6. Once the script finishes, it will update the input CSV file (`results.csv`) with the scraped information. The additional information will be stored in a new column named `Доп. информация`.
-7. You can check the updated CSV file to see the added information.
-
-## Configuration
-
-- `input_file`: Set the path of your input CSV file (default: "results.csv").
-- `new_column`: Set the name of the new column to store the scraped information (default: "Доп. информация").
-- `max_retries`: Set the maximum number of retries for failed requests (default: 3).
+The script will sequentially scrape reviews for each business URL in the input CSV file. It will add the extracted reviews to the specified new column in the CSV file.
 
 ## Notes
 
-- The script handles common exceptions that may occur during the request and retries the failed requests up to the specified number of retries.
-- The script uses a user-agent header to mimic Chrome on macOS for better compatibility with websites.
-- Make sure to respect the website's terms of service and the legal aspects
+- The script uses Selenium with a headless browser to render JavaScript content. The headless mode can be disabled by removing the `--headless` argument from the `Options`.
+- The `max_retries` variable can be adjusted to control the number of retry attempts in case of exceptions while scraping a URL.
+
+## License
+
+This script is licensed under the MIT License. Feel free to modify and use it according to your needs.
--- script.py
+++ script.py
@@ -1,77 +1,190 @@
 import csv
 import time
-import requests
 from bs4 import BeautifulSoup
-import sys
+from datetime import datetime, timedelta
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import locale, re
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Set the input file path
-input_file = "results.csv"
+input_file = "/Users/muzzyaqow/Desktop/updated-database.csv"
+output_file = (
+    "/Users/muzzyaqow/Documents/projects/Gourmenta/server/updated-database.csv"
+)
 
 # Set the new column name
-new_column = "Доп. информация"
+new_column = "Reviews"
 
 # Set the maximum number of retries
 max_retries = 3
 
+# Set the Russian locale for date parsing
+locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
 
-# Function to scrape additional information from the webpage
-def scrape_additional_info(args):
-    row, row_num = args
-    url = row["2GIS URL"] + "/tab/info"
+
+# Function to extract the date from the text with possible additional text
+def extract_date(text):
+    # Regular expression to match the date part (e.g., '23 июля 2023')
+    date_pattern = re.compile(r"\d{1,2}\s\w+\s\d{4}")
+    # Find the date part in the text
+    match = date_pattern.search(text)
+    if match:
+        return match.group()
+    else:
+        return None
+
+
+# Function to parse the date text, handling "сегодня" case
+def parse_date(date_text):
+    if "сегодня" in date_text:
+        # Replace "сегодня" with the current date
+        today = datetime.today()
+        date_text = date_text.replace("сегодня", today.strftime("%d %B %Y"))
+    return date_text
+
+
+def wait_for_element(driver, by, selector, timeout=1):
+    try:
+        return WebDriverWait(driver, timeout).until(
+            EC.visibility_of_element_located((by, selector))
+        )
+    except Exception as e:
+        return None  # Re-raise the exception to propagate it further
+
+
+def scrape_reviews(row, row_num):
+    url = (
+        row.get("2GIS URL") + "/tab/reviews"
+    )  # Use .get() to handle missing key gracefully
+    if not url:
+        print(f"Missing '2GIS URL' in row: {row_num}")
+        return None
+
+    # Set up Selenium with a headless browser
+    options = Options()
+    options.add_argument(
+        "--non-headless"
+    )  # Run the browser in headless mode (optional)
+    options.add_argument("--disable-gpu")  # Disable GPU acceleration (optional)
+    driver = webdriver.Chrome(
+        options=options
+    )  # Change this to the path of your web driver
+
     retries = 0
+    reviews = []
+    last_parsed_date = None  # Keep track of the last parsed date for the row
 
     while retries < max_retries:
         try:
-            headers = {
-                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
-            }
+            driver.get(url)
 
-            response = requests.get(url, headers=headers, verify=True, timeout=10)
-            soup = BeautifulSoup(response.content, "html.parser")
+            while True:
+                # Continue with the rest of the code (parsing reviews and dates)
+                soup = BeautifulSoup(driver.page_source, "html.parser")
+                review_elements = soup.find_all("div", class_="_11gvyqv")
+                today = datetime.today().replace(
+                    hour=0, minute=0, second=0, microsecond=0
+                )
 
-            span_elements = soup.find_all("span", class_="_er2xx9")
-            info = [span.text.strip() for span in span_elements]
+                new_reviews_count = (
+                    0  # Count the number of new reviews loaded in this scroll
+                )
+                for review_element in review_elements:
+                    # Extract review body from the <a> tag
+                    review_body = review_element.find("a").text.strip()
 
-            info_text = " ".join(info)
+                    # Find the date element and extract the date text
+                    date_element = review_element.find("div", class_="_4mwq3d")
+                    date_text = date_element.text.strip()
 
-            row[new_column] = info_text
+                    # Remove the additional text ", отредактирован" if present
+                    date_text = date_text.split(",")[0]
 
-            print(f"Row Number: {row_num}, Name: {row['Наименование']}")
-            sys.stdout.flush()
+                    # Parse the date text, handling "сегодня" case
+                    date_text = parse_date(date_text)
+
+                    # Parse the date using a known format
+                    review_date = datetime.strptime(
+                        extract_date(date_text), "%d %B %Y"
+                    ).replace(hour=0, minute=0, second=0, microsecond=0)
+
+                    # Check if the review is within the last 3 months
+                    if (today - review_date) <= timedelta(days=180):
+                        # Increment the review counter
+                        new_reviews_count += 1
+
+                        # Format the review with the review number
+                        formatted_review = f"Review {new_reviews_count}: {review_body}"
+                        reviews.append(formatted_review)
+
+                        # Update the last parsed date for the row
+                        last_parsed_date = review_date
+                    else:
+                        new_reviews_count = 0
+                        break
+
+                # Check if the "Load more" button is present and wait for it to be visible
+                try:
+                    load_more_button = wait_for_element(
+                        driver, By.XPATH, "//button[contains(text(), 'Загрузить ещё')]"
+                    )
+                except Exception as e:
+                    break
+
+                if load_more_button is None or not load_more_button.is_displayed():
+                    break
+
+                # If all new reviews loaded in this scroll are older than 90 days, break from the loop
+                if new_reviews_count == 0:
+                    break
+
+                driver.execute_script(
+                    "arguments[0].scrollIntoView();", load_more_button
+                )
+                driver.execute_script("arguments[0].click();", load_more_button)
+                time.sleep(0.5)
+                load_more_button = wait_for_element(
+                    driver, By.XPATH, "//button[contains(text(), 'Загрузить ещё')]"
+                )
+
+            row[new_column] = "\n".join(reviews)
+            # Check if last_parsed_date is not None before formatting it
+            if last_parsed_date is not None:
+                print(
+                    f"Row Number: {row_num}, Name: {row.get('Name')}, Last Parsed Date: {last_parsed_date.strftime('%Y-%m-%d')}"
+                )
+
+            driver.quit()  # Close the browser after scraping reviews
 
             return row
         except Exception as e:
             print(f"Exception occurred: {e}")
-            print("Retrying...")
-            sys.stdout.flush()
+            driver.quit()  # Close the browser if an exception occurs
             retries += 1
-            time.sleep(5)
+            time.sleep(1)
 
     print(f"Max retries reached for Row Number: {row_num}")
-    sys.stdout.flush()
     return None
 
 
-# Read the input CSV file
-rows = []
+# Scrape reviews sequentially for each row
+updated_rows = []
 with open(input_file, "r", newline="") as file_in:
     reader = csv.DictReader(file_in)
     fieldnames = reader.fieldnames + [new_column]
 
     for row_num, row in enumerate(reader, start=1):
-        rows.append((row, row_num))
-
-# Scrape additional information
-updated_rows = []
-for i, row in enumerate(rows, start=1):
-    updated_row = scrape_additional_info(row)
-    if updated_row is not None:
-        updated_rows.append(updated_row)
+        updated_row = scrape_reviews(row, row_num)
+        if updated_row is not None:
+            updated_rows.append(updated_row)
 
 # Update the existing CSV file with the modified rows
-with open(input_file, "w", newline="") as file_out:
+with open(output_file, "w", newline="") as file_out:
     writer = csv.DictWriter(file_out, fieldnames=fieldnames)
     writer.writeheader()
     writer.writerows(updated_rows)
 
-print("Additional information has been added to the input CSV file.")
+print("Reviews from the last 6 months have been added to the input CSV file.")
